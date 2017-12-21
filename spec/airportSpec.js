5aa541a146f8fef46735a6e6253cbbc716efe06e
umairb1 <umairbashir95@hotmail.com>
2017-12-21 11:07:30 +0000

    Set capacity for airport

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,39 +1,43 @@
-describe("aiport", function(){
+describe("aiport", function () {
   var plane;
   var airport;
   var weather;
 
-  beforeEach(function(){
+  beforeEach(function () {
     plane = new Plane();
     airport = new Airport();
     weather = new Weather();
   });
 
-  describe("land", function(){
-    it ("should land a plane", function(){
+  describe("land", function () {
+
+    beforeEach(function () {
+      spyOn(Math, 'random').and.returnValue(0);
+    });
+
+    it("should land a plane", function () {
       expect(airport.land(plane)).toEqual(plane)
     });
 
-    it ("should check if the plane is in the terminal", function(){
+    it("should check if the plane is in the terminal", function () {
       airport.land(plane)
       expect(airport.terminal).toEqual(jasmine.arrayContaining([plane]));
     });
   });
 
-  describe("take off", function(){
+  describe("take off", function () {
 
-    beforeEach(function(){
-      spyOn(Math,'random').and.returnValue(0);
+    beforeEach(function () {
+      spyOn(Math, 'random').and.returnValue(0);
     });
 
-    it('should confirm that the plane is no longer at the airport', function(){
+    it('should confirm that the plane is no longer at the airport', function () {
       airport.land(plane)
-      // spyOn(weather, "_isStormy").and.returnValue(false);
       airport.takeOff(plane)
       expect(airport.terminal).not.toContain(plane)
     });
 
-    it('should confirm that the plane has left the terminal', function(){
+    it('should confirm that the plane has left the terminal', function () {
       airport.land(plane);
       // spyOn(weather, "_isStormy").and.returnValue(false);
       expect(airport.takeOff(plane)).toEqual(`${plane} has left the airport.`);
@@ -41,21 +45,31 @@ describe("aiport", function(){
     });
 
 
-    });
-    describe("stormy", function(){
-
-      beforeEach(function(){
-        spyOn(Math,'random').and.returnValue(1);
-      });
-      it('should prevent take off when weather is stormy', function(){
-        airport.land(plane)
-
-        // spyOn(Math,'random').and.returnValue(1);
-        // spyOn(weather, "_isStormy").and.returnValue(true);
-        expect(function(){airport.takeOff(plane);}).toThrowError("Cannot take off when stormy");
-      });
-
   });
 
 
+  describe("when stormy", function () {
+
+    beforeEach(function () {
+      spyOn(Math, 'random').and.returnValue(1);
+    });
+
+    it('should prevent take off when weather is stormy', function () {
+      expect(function () { airport.takeOff(plane); }).toThrowError("Cannot take off when stormy");
+    });
+
+    it('should prevent landing when weather is stormy', function(){
+      expect(function() { airport.land(plane); }).toThrowError("Cannot land when weather is stormy") 
+    });
+
+  });
+
+  describe("terminal is full", function(){
+
+    it("should prevent planes from landing when terminal is full", function(){
+      spyOn(Math, 'random').and.returnValue(0);
+      [1,2,3].forEach(function() { airport.land(plane) });
+      expect(function() { airport.land(plane); }).toThrowError("Airport is full")
+    });
+  });
 });
--- src/airport.js
+++ src/airport.js
@@ -1,17 +1,21 @@
 function Airport(){
   this.terminal = []
   this.weather = new Weather()
+  this.capacity = 3
 };
 
 Airport.prototype.land = function(plane){
+  if(this.weather._isStormy()) {
+    throw new Error("Cannot land when weather is stormy")
+  } else if (this.terminal.length >= this.capacity) {
+    throw new Error("Airport is full")
+  }
   this.terminal.push(plane)
   plane.isNotFlying()
   return plane
 };
 
 Airport.prototype.takeOff = function(plane){
-  // console.log("weather")
-  console.log(this.weather._isStormy())
   if(this.weather._isStormy()) {
     throw new Error("Cannot take off when stormy")
   }
